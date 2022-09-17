aef3e4f12b88ecc56fd7769e9a63dcb726f7ed8f
Giovanni Farfan Briceno <gfarfanb@gmail.com>
2022-09-17 03:21:57 -0500

    Install Emacs in Debian WSL

---
+++ config/custom-vars-windows.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(wombat))
+ '(global-display-line-numbers-mode t)
+ '(org-babel-load-languages '((dot . t) (shell . t) (java . t) (emacs-lisp . t)))
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+     ("melpa" . "http://melpa.org/packages/")))
+ '(projectile-indexing-method 'alien)
+ '(save-place-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
