e01e420e1b1e52cbb884c8c11fa77c17e2026381
Giovanni Farfán B <gfarfanb@gmail.com>
2022-09-29 09:47:38 -0500

    Common scripts

---
+++ scripts/sync-private-pull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp "$SYNC_FOLDER_PRIVATE_DIR"/*.org "$ORG_ROAM_PRIVATE_DIR"
+
+echo "Sync private pull completed"
+
---
+++ scripts/sync-private-push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp "$ORG_ROAM_PRIVATE_DIR"/*.org "$SYNC_FOLDER_PRIVATE_DIR"
+
+echo "Sync private push completed"
+
